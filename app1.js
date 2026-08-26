15b6c9a830dfe9d6a2c706a6714b624c07058176
Beloveddike123 <tobechukwudike95@gmail.com>
2026-08-26 21:35:31 +0100

    my second assignment

---
+++ app1.js
@@ -0,0 +1,9 @@
+const day ="thursday";
+
+const position = "4th";
+
+const sentence = "Today is " + day + ". It is the " + position + "day of the week.";
+
+console.log(day)
+console.log(position)
+console.log(sentence)
