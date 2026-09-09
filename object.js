049c6ad8c89e652414839e5e92a623303a4c1e02
Beloveddike123 <tobechukwudike95@gmail.com>
2026-09-09 09:29:35 +0100

    Remove mistaken instructions from object.js

--- object.js
+++ object.js
@@ -1,10 +1,4 @@
-//Using object literal, write a JavaScript object, books.
-//The book object should take the following properties
-//title (string)
-//description (string)
-//numberOfPages (number)
-//author (string)
-//reading (boolean)
+
 const books ={
     title : "Folake and Her Four Brothers",
     description : "A book about a brave girl and her brothers that didn't allow anyone to intimidate them",
