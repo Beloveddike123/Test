f4e492d546dc1915bd919857ce4fed41843f1a76
Beloveddike123 <tobechukwudike95@gmail.com>
2026-09-09 10:49:10 +0100

    my fifth assignment

--- object.js
+++ object.js
@@ -1,10 +1,10 @@
-
-const books ={
-    title : "Folake and Her Four Brothers",
+const books = {
+    title : "Folake and her four brothers",
     description : "A book about a brave girl and her brothers that didn't allow anyone to intimidate them",
     numberOfPages : 52,
     author : "Ifeanyi Ifoegbuna",
     reading : true
 
 }
- console.log(books)
+
+console.log(books)
