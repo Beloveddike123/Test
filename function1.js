ecfb79975f43199f7a38e8ac3a66946eab3b4911
Beloveddike123 <tobechukwudike95@gmail.com>
2026-09-25 15:29:55 +0100

    my function assignment

---
+++ function1.js
@@ -0,0 +1,33 @@
+// function greet(){
+//  console.log("Good evening, everyone");
+// }
+
+// greet()
+
+// function greetuser(name){
+//     console.log("Hello " + name + " !");
+    
+// }
+// greetuser("Segun")
+
+// function add (a,b){
+//     return a + b 
+// }
+
+// let sum = add(5,10)
+
+// console.log(sum)
+
+// const multiply =(a,b) => a * b
+
+// console.log(multiply(5,10))
+
+// function total(a,b,c){
+//     console.log(a+b+c)
+// }
+// total(10,5,2)
+
+function areaofrectangle(length, width){
+    console.log(length * width);
+}
+areaofrectangle(2,10)
